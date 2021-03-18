65b7005938021eb236bafbd2cfd71d8a0df007ad
nityanantan <bnityanantan@gmail.com>
2021-03-18 22:21:01 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,197 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/react-native-community/react-native-template-typescript
+ *
+ * @format
+ */
+import {
+  ApolloClient,
+  InMemoryCache,
+  gql,
+  ApolloProvider,
+  useQuery,
+} from '@apollo/client';
+
+import React, {useState, useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Image,
+  ActivityIndicator,
+} from 'react-native';
+
+import {Colors, ReloadInstructions} from 'react-native/Libraries/NewAppScreen';
+
+import {FlatListComponent} from './src/components/flatlist';
+
+// const Section: React.FC<{
+//   title: string;
+// }> = ({children, title}) => {
+//   const isDarkMode = useColorScheme() === 'dark';
+//   return (
+//     <View style={styles.sectionContainer}>
+//       <Text
+//         style={[
+//           styles.sectionTitle,
+//           {
+//             color: isDarkMode ? Colors.white : Colors.black,
+//           },
+//         ]}>
+//         {title}
+//       </Text>
+//       <Text
+//         style={[
+//           styles.sectionDescription,
+//           {
+//             color: isDarkMode ? Colors.light : Colors.dark,
+//           },
+//         ]}>
+//         {children}
+//       </Text>
+//     </View>
+//   );
+// };
+
+const App = () => {
+  
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState<any>([]);
+  const [error, setError] = useState(null);
+  const [fullData, setFullData] = useState<any>([]);
+
+  const client = new ApolloClient({
+    uri: 'https://api.graphql.jobs/graphql',
+    cache: new InMemoryCache(),
+  });
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    client
+      .query({
+        query: gql`
+          query jobs {
+            jobs {
+              locationNames
+            }
+          }
+        `,
+      })
+      .then(response => response)
+      .then(response:Article => {
+        console.log(response?.jobs);
+        setData(response);
+
+        // ADD THIS
+        setFullData(response);
+
+        setIsLoading(false);
+      })
+      .catch(err => {
+        setIsLoading(false);
+        setError(err);
+      });
+  }, []);
+
+  //   const sample = client.query({
+  //     query: gql`
+  //       query jobs {
+  //         jobs {
+  //           locationNames
+  //         }
+  //       }
+  //     `,
+  //   });
+
+  //   setData(sample?)
+  //   .then(result => setData(result.results
+  //         setFullData(response.results);
+  //           setIsLoading(false);
+
+  //   );
+  // ));
+
+  const isDarkMode = useColorScheme() === 'dZark';
+
+  const backgroundStyle = {
+    backgroundColor: isDarkMode ? Colors.lighter : Colors.lighter,
+  };
+
+  return (
+    <ApolloProvider client={client}>
+      <SafeAreaView style={backgroundStyle}>
+        <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
+        <Text style={styles.text}>Favorite Contacts</Text>
+
+        {/* <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={backgroundStyle}> */}
+        <View
+          style={{
+            backgroundColor: isDarkMode ? Colors.lighter : Colors.white,
+          }}>
+          <FlatListComponent />
+        </View>
+        {/* </ScrollView> */}
+      </SafeAreaView>
+    </ApolloProvider>
+  );
+};
+
+const styles = StyleSheet.create({
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+  text: {
+    fontSize: 20,
+    color: '#101010',
+    marginTop: 60,
+    alignItems: 'center',
+    alignSelf: 'center',
+    fontWeight: '700',
+  },
+  listItem: {
+    marginTop: 10,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+  },
+  coverImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  metaInfo: {
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 18,
+    width: 200,
+    padding: 10,
+  },
+});
+
+export default App;
---
+++ src/components/flatlist.tsx
@@ -0,0 +1,71 @@
+import {gql, useQuery} from '@apollo/client';
+import React from 'react';
+import {StyleSheet, Text, View, FlatList, Image} from 'react-native';
+
+const sample: any = [
+  {id: '1', title: 'First item'},
+  {id: '2', title: 'Second item'},
+  {id: '3', title: 'Third item'},
+  {id: '4', title: 'Fourth item'},
+];
+
+export const FlatListComponent: React.FC<{data: any}> = ({data}) => {
+  const GET_JOBS = gql`
+    query jobs {
+      jobs {
+        id
+        title
+      }
+    }
+  `;
+
+  //   const {data}: any = useQuery(GET_JOBS);
+  const sub = data;
+
+  //   console.log(data?.jobs);
+
+  return (
+    <FlatList
+      nestedScrollEnabled
+      data={sub}
+      keyExtractor={item => item.id}
+      renderItem={({item}) => (
+        <View style={styles.listItem}>
+          <Image source={{uri: item.title}} style={styles.coverImage} />
+          <View style={styles.metaInfo}>
+            <Text style={styles.title}>{`${item?.title}`}</Text>
+          </View>
+        </View>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  text: {
+    fontSize: 20,
+    color: '#101010',
+    marginTop: 60,
+    fontWeight: '700',
+  },
+  listItem: {
+    marginTop: 10,
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+  },
+  coverImage: {
+    width: 100,
+    height: 100,
+    borderRadius: 8,
+  },
+  metaInfo: {
+    marginLeft: 10,
+  },
+  title: {
+    fontSize: 18,
+    width: 200,
+    padding: 10,
+  },
+});

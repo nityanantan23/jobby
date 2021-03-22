47e2191fa101ea488df06783aae06c0c7f3c898b
nityanantan23 <48348087+nityanantan23@users.noreply.github.com>
2021-03-22 20:44:35 +0800

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -1,12 +1,4 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * Generated with the TypeScript template
- * https://github.com/react-native-community/react-native-template-typescript
- *
- * @format
- */
+
 import {ApolloClient, InMemoryCache, ApolloProvider} from '@apollo/client';
 import React, {useState} from 'react';
 import Search from './src/components/search/search';
